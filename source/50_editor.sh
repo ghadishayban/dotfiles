81a43337368eafa75693ce03a7564534daad0336
Ghadi Shayban <gshayban@gmail.com>
2012-12-31 14:47:51 -0500

    unset sublime text editor

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,13 +1,13 @@
 # Editing
 
-if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
-  export EDITOR='subl -w'
-  export LESSEDIT='subl %f'
-  alias q='subl'
-else
-  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
-  alias q="$EDITOR -w -z"
-fi
+#if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
+#  export EDITOR='subl -w'
+#  export LESSEDIT='subl %f'
+#  alias q='subl'
+#else
+#  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
+#  alias q="$EDITOR -w -z"
+#fi
 
 export VISUAL="$EDITOR"
 
