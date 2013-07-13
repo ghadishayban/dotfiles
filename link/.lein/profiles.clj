329dfe42b12f332a8a2def8678f6932bd2208a5f
Ghadi Shayban <gshayban@gmail.com>
2013-07-13 13:33:37 -0400

    add .bin to path, add lein 2.2

--- link/.lein/profiles.clj
+++ link/.lein/profiles.clj
@@ -1,5 +1,8 @@
-{:user {:plugins [[lein-localrepo "0.4.1"]]
-        :dependencies [[clj-stacktrace "0.2.5"]]
+{:user {:plugins [[lein-localrepo "0.5.2"]
+                  [lein-ring "0.8.6"]
+                  ;[lein-midje "2.0.4"]
+]
+        :dependencies [[clj-stacktrace "0.2.6"]]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
                            new (ns-resolve (doto 'clj-stacktrace.repl require)
